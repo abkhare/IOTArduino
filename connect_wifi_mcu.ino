41551f1f77d7781bfe2c5f2b5dec307c1f1c80d5
Abhiraj Khare <abhi@Abhirajs-MacBook-Pro.local>
2017-01-27 04:10:29 +0530

    first commit

---
+++ connect_wifi_mcu.ino
@@ -0,0 +1,91 @@
+/*
+ *  This sketch sends data via HTTP GET requests to data.sparkfun.com service.
+ *
+ *  You need to get streamId and privateKey at data.sparkfun.com and paste them
+ *  below. Or just customize this script to talk to other HTTP servers.
+ *
+ */
+
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "Airtel";
+const char* password = "router@home";
+
+const char* host = "iot-android.000webhostapp.com";
+const char* streamId   = "";
+const char* privateKey = "";
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(13, OUTPUT);   // D7 for NodeMCU
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+int value = 0;
+
+void loop() {
+  delay(1000);
+  ++value;
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+  
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  
+  // We now create a URI for the request
+  String url = "/response.txt";
+  
+  Serial.print("Requesting URL: ");
+  //Serial.println(url);
+  
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  delay(10);
+  
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    int index = line.indexOf("\"status\":");
+    String stat = line.substring(index+9, index+10);
+    if(stat.equals("1")) {
+      Serial.print(stat);
+      digitalWrite(13, HIGH);
+    }
+    else if(stat.equals("0")) {
+      Serial.print(stat);
+      digitalWrite(13, LOW);
+    }
+    
+  }
+  
+  Serial.println();
+  Serial.println("closing connection");
+}
+
